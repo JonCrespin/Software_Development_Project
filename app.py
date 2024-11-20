264c24531d918cac8a1cb2c810f608183ab4480a
JonCrespin <JonCrespin@proton.me>
2024-11-20 13:31:19 -0700

    Update Streamlit dashboard with interactive sales region selection

--- app.py
+++ app.py
@@ -7,3 +7,35 @@ df= pd.read_csv('vgchartz_2024.csv')
 
 st.header("Video Game Sales Dashboard")
 st.write("Explore video game sales by platform, genre, and region!")
+
+df= df.drop(df.columns[0], axis =1)
+df['release_date'] = pd.to_datetime(df['release_date'], format='%Y-%m-%d', errors='coerce')
+df['last_update'] = pd.to_datetime(df['last_update'], format='%Y-%m-%d', errors='coerce')
+
+
+start_year, end_year = 2000, 2024
+console_choice = df[
+    (df['release_date'].dt.year >= start_year) & 
+    (df['release_date'].dt.year <= end_year)
+]['console'].unique()
+console_choice.sort()
+selected_menu = st.selectbox('Select an console',console_choice)
+
+df_filtered = df[(df.console == selected_menu) ]
+                 
+st.dataframe(df_filtered)
+
+st.header('Distribution of Price per Genre per Region')
+st.write("""
+###### Let's analyze what influences price the most. We will check how distribution of price varies depending on genre per region.
+""")
+
+sales_columns = ['na_sales', 'jp_sales', 'pal_sales', 'other_sales']
+
+selected_region = st.selectbox('Split for price distribution',sales_columns)
+
+
+fig1 = px.scatter(df_filtered, x='genre', y=selected_region, color='genre', title=f'{selected_region} Distribution by Genre')
+fig1.update_layout(title= "<b> Split of price by {}</b>".format(selected_region))
+
+st.plotly_chart(fig1)
